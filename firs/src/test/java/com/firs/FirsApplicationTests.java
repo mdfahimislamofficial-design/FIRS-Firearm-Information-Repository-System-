ca94a69e59c8c1c668972805bd230800b24168a3
Md Fahim Islam <md.fahim.islam.official@gmail.com>
2026-04-12 22:01:58 +0600

    Add files via upload

---
+++ firs/src/test/java/com/firs/FirsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.firs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
