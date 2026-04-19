0e53a6d45c892fda30688830f3fa704e53d21d7b
Md Fahim Islam <md.fahim.islam.official@gmail.com>
2026-04-19 11:31:19 +0600

    Delete firs directory

--- firs/src/main/java/com/firs/repository/OrderItemRepository.java
+++
@@ -1,9 +0,0 @@
-package com.firs.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.firs.model.OrderItem;
-import java.util.List;
-
-public interface OrderItemRepository extends JpaRepository<OrderItem, Long> {
-    List<OrderItem> findByOrderId(Long orderId);
-}
